04864947c948bf0f2de059ef23e4650aa412f5dd
Erik Jeffery <JeffErik143@GoTVDSB.ca>
2021-11-02 14:34:15 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_9___Making_a_class__Console_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student person = new Student("Robert", "Ross");
+            Console.WriteLine(person.firstName);
+            person.firstName = "Bob";
+            Console.WriteLine(person.firstName);
+            Console.WriteLine(person.lastName);
+            person.lastName = "Boss";
+            Console.WriteLine(person.lastName);
+            Console.WriteLine(person.StudentNumber);
+            Console.WriteLine(person);
+            Console.WriteLine(person.Email);
+            Console.WriteLine("--------------------------------------------------------------------------");
+            List<Student> students = new List<Student>();
+            students.Add(new Student("Bob", "Ross"));
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine("Enter the Student's first name");
+                string firstname = Console.ReadLine();
+
+                Console.WriteLine("Student's last name");
+                string lastname = Console.ReadLine();
+
+                students.Add(new Student(firstname, lastname));
+            }
+            for (int i = 0; i <=3; i++)
+            {
+                Console.WriteLine(students[i]);
+            }
+
+
+
+            Console.WriteLine("That's all folks, press ENTER to leave");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_9___Making_a_class__Console_
+{
+    class Student
+    {
+        private static Random generator = new Random();
+        private string _firstname;
+        private string _lastname;
+        private int _studentnumber;
+        private string _email;
+
+        public Student(string firstName, string lastName)
+        {
+            //instance variable, fields initialized here
+            _firstname = firstName;
+            _lastname = lastName;
+
+            _studentnumber = generator.Next(100, 1000);
+            generateEmail();
+        }
+
+        public string firstName
+        {
+            get
+            {
+
+                return _firstname;
+
+            }
+            set
+            {
+
+                _firstname = value;
+                generateEmail();
+            }
+        }
+        public string lastName
+        {
+            get
+            {
+                return _lastname;
+            }
+            set
+            {
+                _lastname = value;
+                generateEmail();
+            }
+
+
+
+        }
+        public int StudentNumber
+        {
+            get
+            {
+                return _studentnumber;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+
+
+        }
+
+
+        public override string ToString()
+        {
+            return _firstname + " " + _lastname;
+        }
+        private void generateEmail()
+        {
+            //first 3 letters of first name, first 3 letters of the last name, and the student #, @ICS4U.ca
+            _email = _firstname.Substring(0, 3) + _lastname.Substring(0, 3) + _studentnumber + "@ICS4U.ca";
+            
+
+
+
+        }
+
+    }
+}
